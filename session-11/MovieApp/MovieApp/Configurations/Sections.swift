b3e8bae5604a476ee9ac48c0b64a448dcb706f75
jmayta1984 <jmayta@pucp.edu.pe>
2024-11-26 10:48:08 -0500

    feat: udpate movie view

---
+++ session-11/MovieApp/MovieApp/Configurations/Sections.swift
@@ -0,0 +1,8 @@
+//
+//  Sections.swift
+//  MovieApp
+//
+//  Created by DAMII on 26/11/24.
+//
+
+import Foundation
---
+++ session-11/MovieApp/MovieApp/Views/MoviesView.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesView.swift
+//  MovieApp
+//
+//  Created by DAMII on 26/11/24.
+//
+
+import SwiftUI
+
+struct MoviesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoviesView()
+}
